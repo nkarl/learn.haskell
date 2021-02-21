b420f30be48b659ebaea79edb09b1d02b63f6410
Karl <redking.alp@protonmail.com>
2021-02-20 20:03:50 -0800

    updated with practice variations for recursion

--- deep-practice/recursion-practice.hs
+++ deep-practice/recursion-practice.hs
@@ -4,22 +4,92 @@
 -- optimization in a wider array of functions. These functions will perform more diverse tasks while serving
 -- as a good examples for how we can use recursion in Haskell.
 --
+--
+--------------------------------------------------------------------------------------------------------------
 -- 1. inSert:
---    Write a function inSert that:
---      - takes an integer 'n', a value 'item' and a list 'iL', and then
---      - inserts the 'item' at index 'n' in the list 'iL'.
+--------------------------------------------------------------------------------------------------------------
+-- Write a function inSert that:
+--   - takes an integer 'n', a value 'item' and a list 'iL', and then
+--   - inserts the 'item' at index 'n' in the list 'iL'.
 --
---    Other considerations:
---      - 'n' is a 1-based index, i.e. 'item' should be inserted after n'th element in the list.
---      - The type signature of inSert can be:
+aa      = [1,2,3,4,5,6]
+n       = 3
+item    = 999
+--------------------------------------------------------------------------------------------------------------
+-- Other considerations:
+--   - 'n' is a 1-based index, i.e. 'item' should be inserted after n'th element in the list.
+--   - The type signature of inSert can be:
 --
---          insert :: (Num t1) => t1 -> t2 -> [t2] -> [t2]
+--           insert :: (Num t1) => t1 -> t2 -> [t2] -> [t2]
 --      
---        or,
+--     or,   insert :: (Eq t1, Num t1) => t1 -> t2 -> [t2] -> [t2]
 --
---          insert :: (Eq t1, Num t1) => t1 -> t2 -> [t2] -> [t2]
+--     or,   insert :: (Ord t1, Num t1) => t1 - t2 -> [t2] -> [t2]
 --
---        or,
+--------------------------------------------------------------------------------------------------------------
+-- Let's not worry about the type signature for now. Let's think about the regular case and the recursive
+-- case first, i.e. what we want the function to behave.
 --
---          insert :: (Ord t1, Num t1) => t1 - t2 -> [t2] -> [t2]
+-- First, we implement the basic recursion:
 --
+-- Assume a list aa == [1,2,3,4,5], insert at n=3 the item=999 into the list aa.
+--
+-- In the regular case, that is the list is not empty, we have to:
+--
+--      - take a list, break it up recursively until
+--      - we arrive at the correct index 'n', at which point
+--      - we attach the 'item' to the list, and then
+--      - attach the 'rest' to the new list.
+--
+-- We look through the steps above and see that we need (x:xs), a sentinel 'n' and an 'item'.
+--
+--          insert n item (x:xs) = x:(insert (n-1) item xs)
+--
+-- Eventually, we reach the base case. But we need to be careful here. What should be our base case?
+-- Obviously we cannot continue passing the list until it is empty like in the cpString example. In such a
+-- case, the sentinel 'n' will continue to be subtracted past zero and become negative.
+-- 
+-- Thus, our base case must be when 'n' reaches 0.
+-- 
+--          insert 0 item (x:xs) = xs
+--          insert n item (x:xs) = x:(insert (n-1) item xs)
+-- 
+-- With the implementation above, we will get [1,2,3,5] as result because we forgot to attach item to the
+-- 'rest'. Let's improve it:
+--
+--          insert 0 item (x:xs) = item:xs
+--          insert n item (x:xs) = x:(insert (n-1) item xs)
+--
+-- With this improvement, we will get [1,2,3,999,5] as result, which means we have successfully inserted the
+-- item=999 into a list. Although, this list not exactly the original list because it is missing the value 4.
+-- Thus we must make another improvement:
+--
+--          insert 0 item (x:xs) = item:x:xs
+--          insert n item (x:xs) = x:(insert (n-1) item xs)
+--          
+-- Now, the function behaves correctly as we want it to!
+--
+--------------------------------------------------------------------------------------------------------------
+-- Now let's go back to the prompt and look at a few important details.
+--
+-- 1. The index 'n' is 1-based. What does this mean? It means that the item should be inserted AFTER the n'th
+--      element.
+--      
+--          [1, 2, 3,  , 4, 5, 6]
+--           ^  ^  ^  ^
+--           |  |  |  |
+--           3  2  1  0
+--                    insert here
+--
+--
+-- 2. Let's rewrite it with tail optimization. We obviously need a bucket to store the 'heads' and we also
+--      need to remember to reverse the list once done. First we add a bucket:
+--
+--          insert 0 item (x:xs) bucket = item:x:bucket
+--          insert n item (x:xs) bucket = insert (n-1) item xs (x:bucket)
+insert 0 item (x:xs) bucket = x:item:bucket
+insert n item (x:xs) bucket = insert (n-1) item xs (x:bucket)
+--
+--
+--    With this implementation, we will get [4, 999, 3, 2, 1]. Notice that we are missing the 'rest' of the
+--    original list. How do solve this problem?
